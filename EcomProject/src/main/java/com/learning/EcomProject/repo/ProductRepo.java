fb8393bf0665294b21d5e970c285e340b3a87195
Harsh Sahu <harshsahu1109@gmail.com>
2026-06-10 09:49:08 +0530

    Backend Ecommerce CRUD

---
+++ EcomProject/src/main/java/com/learning/EcomProject/repo/ProductRepo.java
@@ -0,0 +1,10 @@
+package com.learning.EcomProject.repo;
+
+import com.learning.EcomProject.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product,Integer> {
+
+}
